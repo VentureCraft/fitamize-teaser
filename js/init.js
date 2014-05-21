742c5eafcdd361b486b0b69147b059dff9bd292f
Matt Trimarchi <mat@venturecraft.com.au>
2014-05-21 23:15:29 +1000

    updates
    + omg im awesome
    + mobile responsive
    + all the spacing
    + nice stuff

--- js/init.js
+++ js/init.js
@@ -31,9 +31,9 @@
 				breakpoints: {
 					//'widest': { range: '*', containers: 1140, hasStyleSheet: false },
 					//'wide': { range: '-1680', containers: 960 },
-					//'normal': { range: '-1080', containers: '95%' },
+					'normal': { range: '-1280', containers: '95%',grid: {}},
 					//'narrow': { range: '-840', containers: '95%', grid: { gutters: 30 } },
-					//'mobile': { range: '-640', lockViewport: true, containers: '95%', grid: { collapse: true, gutters: 20 } }
+					'mobile': { range: '-640', lockViewport: true, containers: '90%', grid: { collapse: true, gutters: 20 } }
 				}
 			},
 
